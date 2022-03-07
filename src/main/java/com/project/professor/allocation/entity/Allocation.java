01770e59d9d452bacde85b6890137e12efeb96ae
Kleyson Clementino <kleyson.ctg@gmail.com>
2022-03-07 17:59:31 -0300

    Add Getter and Setter

--- src/main/java/com/project/professor/allocation/entity/Allocation.java
+++ src/main/java/com/project/professor/allocation/entity/Allocation.java
@@ -96,11 +96,29 @@ public class Allocation {
 		this.professorId = professorId;
 	}
 
+	public Professor getProfessor() {
+		return professor;
+	}
+
+	public void setProfessor(Professor professor) {
+		this.professor = professor;
+	}
+
+	public Course getCourse() {
+		return course;
+	}
+
+	public void setCourse(Course course) {
+		this.course = course;
+	}
+
 	@Override
 	public String toString() {
 		return "Allocation [id=" + id + ", day=" + day + ", start=" + start + ", end=" + end + ", courseId=" + courseId
 				+ ", professorId=" + professorId + ", professor=" + professor + ", course=" + course + "]";
 	}
+
+
 	
 	
 }
